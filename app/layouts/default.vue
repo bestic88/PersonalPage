8b67b9a143b84b821f687798d978d055f29134fb
Bestic <bestji@seznam.cz>
2023-12-14 11:52:18 +0100

    Tailwind fixed

---
+++ app/layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="flex flex-col h-screen">
+    <div class="navbar bg-base-200">
+      <div class='flex-1'>
+        <NuxtLink to="/" class="btn btn-ghost text-primary normal-case text-xl">RiDara</NuxtLink>
+      </div>
+      <div class='flex-none'>
+<!--        <ThemeToggle/>-->
+      </div>
+    </div>
+
+    <div class="bg-base-100 h-full">
+      <slot />
+    </div>
+
+<!--    <LayoutFooter/>-->
+  </div>
+</template>
+<script setup lang="ts">
+</script>
