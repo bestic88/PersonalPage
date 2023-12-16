d2def2709d3b58cebbc4ca78d819e753d9bb24d2
Bestic <bestji@seznam.cz>
2023-12-16 20:20:20 +0100

    CV start

--- app/pages/personalResume.vue
+++ app/pages/personalResume.vue
@@ -3,13 +3,102 @@
 </script>
 
 <template>
-  <div class="hero bg-base-200">
-    <div class="hero-content flex-col lg:flex-row-reverse">
-      <img src="../public/pictures/face.jpg" class="max-w-sm rounded-lg shadow-2xl" />
+  <div class="bg-base-200">
+    <div class="container mx-auto">
+      <div class="hero-content flex-col lg:flex-row-reverse">
+        <img src="/pictures/face.jpg" class="max-w-sm rounded-lg shadow-2xl"/>
+        <div>
+          <h1 class="text-4xl font-bold">Ing. Jiří Besta</h1>
+          <p class="py-6">My name is Jiří Besta I am 24 years old. Currently I live in Mladá Boleslav and I am a
+            software
+            developer and also professional floorball player. I am a graduate of the VSB - Technical University of
+            Ostrava
+            in the field of software engineering.</p>
+
+          <h2 class="text-2xl font-bold">Basic information</h2>
+          <div class="mt-4 flex items-center">
+            <span class="i-mdi-calendar-month text-xl"></span>
+            <p class="ml-2">Birthday: 19.4.1999</p>
+          </div>
+          <div class="mt-4 flex items-center">
+            <span class="i-mdi-phone text-xl"></span>
+            <p class="ml-2">Phone: +420 777 781 718</p>
+          </div>
+          <div class="mt-4 flex items-center">
+            <span class="i-mdi-email text-xl"></span>
+            <p class="ml-2">Email: bestji@seznam.cz</p>
+          </div>
+          <div class="mt-4 flex items-center">
+            <span class="i-mdi-place text-xl"></span>
+            <p class="ml-2">Living: Mladá Boleslav/Ostrava</p>
+          </div>
+          <div class="mt-4 flex items-center">
+            <span class="i-mdi-drivers-license text-xl"></span>
+            <p class="ml-2">Driving license: B</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="container mx-auto">
+    <div class=" p-4">
       <div>
-        <h1 class="text-5xl font-bold">Ing. Jiří Besta</h1>
-        <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-        <button class="btn btn-primary">Get Started</button>
+        <h1 class="text-2xl font-bold mb-4">Work experience</h1>
+        <h3 class="text-lg font-semibold">Web application development</h3>
+        <p class="text-sm w-100 text-gray-600">Hubio s.r.o, Ostrava (2020- present)</p>
+        <div class="p-4">
+          <ul class="list-disc pl-4">
+            <li>Software engineering application in the development of websites and applications using modern web
+              technologies.
+            </li>
+            <li>Customer communication to gather initial requirements and ongoing consultation with stakeholders during
+              system development.
+            </li>
+            <li>Modification and maintenance of created systems.</li>
+          </ul>
+        </div>
+
+        <h3 class="text-lg font-semibold">Shop manager</h3>
+        <p class="text-sm w-100 text-gray-600">4sport-florbal, Ostrava (2018-2021)</p>
+        <div class="p-4">
+          <ul class="list-disc pl-4">
+            <li>Management of the assortment in the floorball store.</li>
+            <li>Selling assortment in a floorball store.</li>
+            <li>Communication with customers and establishing business cooperation.</li>
+            <li>Stand sales at various sports events.</li>
+          </ul>
+        </div>
+
+        <h3 class="text-lg font-semibold">IT experience</h3>
+        <p class="text-sm w-100 text-gray-600">RELIGIS s.r.o, Ostrava (2017)</p>
+        <div class="p-4">
+          <ul class="list-disc pl-4">
+            <li>Professional experience during studies at the Secondary Industrial School of Electrical Engineering and
+              Informatics in Ostrava.
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="bg-base-200">
+    <div class="container mx-auto">
+      <div class=" p-4">
+        <div>
+          <h1 class="text-2xl font-bold mb-4">Education</h1>
+          <h3 class="text-lg font-semibold">Bc, VSB - Technical University of Ostrava</h3>
+          <p class="text-sm w-100 text-gray-600">(2018- 2021)</p>
+          <div class="mt-2">
+            <p>Successfully completed bachelor's full-time study at the prestigious VSB - Technical University of Ostrava.</p>
+          </div>
+
+          <h3 class="text-lg font-semibold mt-4">Ing, VSB - Technical University of Ostrava</h3>
+          <p class="text-sm w-100 text-gray-600">(2021- 2023)</p>
+          <div class="mt-2">
+            <p>Successfully completed master's full-time study focused on software engineering.</p>
+          </div>
+
+        </div>
       </div>
     </div>
   </div>
--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,11 +1,12 @@
 import {iconsPlugin} from "@egoist/tailwindcss-icons";
 
 /** @type {import('tailwindcss').Config} */
+const { iconsPlugin, getIconCollections } = require("@egoist/tailwindcss-icons")
 export default {
   plugins: [require("daisyui"), iconsPlugin({
     // Select the icon collections you want to use
     // You can also ignore this option to automatically discover all icon collections you have installed
-    collections: getIconCollections(["mdi", "lucide"]),
+    //collections: getIconCollections(["mdi", "lucide"]),
   }),],
   content: [],
   theme: {
